a2dd81d86d73664cfd71371c50ea859a48a54eb8
doroguba <doroguba@dodoguba.xyz>
2016-08-29 18:44:28 +0900

    modified:   app/Controller/DeployController.php 	new file:   app/tmp/cache/persistent/myapp_cake_core_cake_dev_ja 	modified:   app/tmp/cache/persistent/myapp_cake_core_file_map 	modified:   app/tmp/logs/error.log

--- app/Controller/DeployController.php
+++ app/Controller/DeployController.php
@@ -1,9 +1,10 @@
 <?php
 
-class DeployController extends Controller {
+class DeployController extends AppController {
 
 	public function index(){
 
+	 exec('cd /var/www/html/test;git pull origin master');
 
 	}
 
